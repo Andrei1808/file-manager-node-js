d5e8d91233e65f3702677c50736c54e6b9cf4a59
Andrei <kaliarovich93@gmail.com>
2024-02-01 23:57:06 +0200

    feat: add close options

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import os from 'os'
-import process from 'process'
-import userName from './handlers/userName.js'
-import { createInterface } from 'readline'
+import os from 'os';
+import process from 'process';
+import { argv } from 'process';
+import { createInterface } from 'readline';
+import userName from './handlers/userName.js';
+import { read } from 'fs';
 
 async function App() {
-  process.chdir(os.homedir())
-  console.info(`You are currently in ${process.cwd()}`)
-  userName()
+  process.chdir(os.homedir());
+   console.info(`You are currently in ${process.cwd()}`);
+   
+  const user = argv[argv.length - 1].replace('--username=', '').trim();
+  userName(user);
 
   const readline = createInterface({
     input: process.stdin,
     output: process.stdout,
-  })
+  });
+   
+
+  readline
+     .on('SIGINT', () => readline.close())
+     .on('line', (input) => {
+      if(input === '.exit') readline.close()
+    })
+    .on('close', () => {
+      console.log(`Thank you for using File Manager, ${user}!`);
+    });
 }
 
-App()
+App();
