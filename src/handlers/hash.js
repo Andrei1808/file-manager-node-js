8779bbfda59800e4d281f41ed01ebcf080a3b01e
Andrei <kaliarovich93@gmail.com>
2024-02-05 22:32:16 +0200

    fat: add hash handler

--- src/handlers/hash.js
+++ src/handlers/hash.js
@@ -1,5 +1,7 @@
 import process from 'process';
 import path from 'path';
+import { createHash } from "crypto";
+import fs from 'fs';
 
 export default async function Hash(input) {
   const files = input.replace('hash', '').trim();
@@ -7,12 +9,21 @@ export default async function Hash(input) {
 
   const currFile = inputArray[0];
   const pathToCurrentFile = path.resolve(process.cwd(), currFile);
+    
+    try {
+        const hash = createHash("sha256");
+  const readStream = fs.createReadStream(pathToCurrentFile);
+  readStream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
 
-  try {
-
+  readStream.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
   } catch (error) {
-    console.error('Operation failed');
+        console.error('Operation failed');
   } finally {
     console.info(`You are currently in ${process.cwd()}`);
   }
+
 }
